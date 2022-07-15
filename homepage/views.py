a8486dba0f1ffa624830e01359fd712c96581980
Mohammad Kaif Jaid Shaikh <shaikh.jaid.20@mountblue.tech>
2022-07-15 11:58:22 +0530

    Added .env

--- homepage/views.py
+++ homepage/views.py
@@ -8,7 +8,7 @@ def homepage(request):
     if request.user.is_authenticated:
         print('Yesssssssssssssssssssss')
         print(f"Username --> {request.user.username}")
-        return render(request, 'homepage/index.html')
+        return render(request, 'homepage/loggedin.html')
     else:
         print('Noooooooooooooooooooooo')
         return render(request, 'homepage/non_loggedin.html')
--- mytube/settings.py
+++ mytube/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from ctypes import cast
 from pathlib import Path
-
+from decouple import config
+from numpy import integer
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1s4r95d704g65g735*$qt7!9mg@4b%eym7(%6lmt30g2%c+65p'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +82,7 @@ DATABASES = {
         'USER': 'mytube_admin',
         'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': config('PORT'),
     }
 }
 
