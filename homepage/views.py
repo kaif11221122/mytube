f7ee491baa5df0b38a5952b1cc1809f71d600d6d
Mohammad Kaif Jaid Shaikh <shaikh.jaid.20@mountblue.tech>
2022-07-15 15:56:08 +0530

    Added connection to Postgresql database of Heroku, Both databases working fine and static files are also laoding properly and I don't need to change the code for running in local or in heroku platform.

--- homepage/views.py
+++ homepage/views.py
@@ -6,9 +6,6 @@ from django.shortcuts import render
 
 def homepage(request):
     if request.user.is_authenticated:
-        print('Yesssssssssssssssssssss')
-        print(f"Username --> {request.user.username}")
         return render(request, 'homepage/loggedin.html')
     else:
-        print('Noooooooooooooooooooooo')
         return render(request, 'homepage/non_loggedin.html')
--- mytube/urls.py
+++ mytube/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('user_authentication.urls')),
+    path('', include('user_authentication.urls')),
     path('', include('homepage.urls'), name='home'),
 ]
