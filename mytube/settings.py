97911c0ca9bc6102d99c603bcb0059bbc11bacfd
Mohammad Kaif Jaid Shaikh <shaikh.jaid.20@mountblue.tech>
2022-07-15 15:23:03 +0530

    Added connection to Heroku Postgres Database

--- mytube/settings.py
+++ mytube/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
