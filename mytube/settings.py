a78344a328d3209cfbf2426c459e901aab71593c
Mohammad Kaif Jaid Shaikh <shaikh.jaid.20@mountblue.tech>
2022-07-15 15:50:45 +0530

    change in setting for allowed-hosts

--- mytube/settings.py
+++ mytube/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['mytube0.herokuapp.com']
+ALLOWED_HOSTS = ['mytube0.herokuapp.com', 'locahost', '127.0.0.1']
 
 # Application definition
 
