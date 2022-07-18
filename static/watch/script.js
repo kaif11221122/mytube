3f605487ccf7129441627e69789fa1804265e692
Mohammad Kaif Jaid Shaikh <shaikh.jaid.20@mountblue.tech>
2022-07-18 13:25:08 +0530

    Updated video page
    Added toggle for side-bar on video page
    Added side-bar video linking to open respective pages

---
+++ static/watch/script.js
@@ -0,0 +1,8 @@
+var menu_icon = document.querySelector(".menu-icon");
+var side_bar = document.querySelector(".sidebar");
+var container = document.querySelector(".play-container");
+
+menu_icon.onclick = function(){
+    side_bar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
+}
